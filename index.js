cd4893cf514f1222a58d1bc26ebfe664ede6c218
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-03 07:52:01 +0300

    Adds arithmetic messages

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+//=> Random number (2)
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+let numbers = [10, 15, 20, 5, 12];
+let max = Math.max(...numbers);
+
+console.log(max); // Output: 20
